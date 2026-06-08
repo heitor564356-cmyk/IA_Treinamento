94dc6737d0c90bbeb9ea2f89bf37fb5c462f9a67
heitor564356-cmyk <heitor564356@gmail.com>
2026-06-08 19:59:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -688,4 +688,7 @@ if __name__ == "__main__":
     print("  SISTEMA DA EVOLUÇÃO — Backend v1.0")
     print("  Servidor: http://localhost:5000")
     print("=" * 50)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+   import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
